5503142f9c86c9315b5086587d3d63f7a80d2481
KyunKim <121778563+KyunKim@users.noreply.github.com>
2023-01-05 14:37:45 +0900

    Initial commit

---
+++ src/main/java/com/istay/ecstestapplication/EcsTestApplication.java
@@ -0,0 +1,13 @@
+package com.istay.ecstestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcsTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/istay/ecstestapplication/controller/ECSTestController.java
@@ -0,0 +1,20 @@
+package com.istay.ecstestapplication.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ECSTestController {
+
+    @GetMapping("/main")
+    public String getMain() {
+        return "main";
+    }
+
+
+
+
+}
