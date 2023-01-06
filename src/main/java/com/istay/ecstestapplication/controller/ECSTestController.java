7b379fcc266fd716bb459f33ab2bc62284d3b58c
KyunKim <121778563+KyunKim@users.noreply.github.com>
2023-01-06 16:38:47 +0900

    추가 API 생성

--- src/main/java/com/istay/ecstestapplication/controller/ECSTestController.java
+++ src/main/java/com/istay/ecstestapplication/controller/ECSTestController.java
@@ -3,6 +3,7 @@ package com.istay.ecstestapplication.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +26,10 @@ public class ECSTestController {
         return "test-2 success";
     }
 
+
+    @GetMapping("/test/main/{str}")
+    public String getStringTestPrint(@PathVariable String str) {
+        return "input Path : variable is " + str;
+    }
 
 }
